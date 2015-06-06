a569695cef09754d334c511581f7436ac44a8706
seddonma <matthew.seddon@outlook.com>
2015-06-06 12:20:05 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -17,4 +17,9 @@ library(lubridate)
 dataset$Date <- dmy(dataset$Date)
 dataset <- subset(dataset, dataset$Date == ymd("2007-02-01") | dataset$Date == ymd("2007-02-02"))
 
-
+## Prepares data for graphing and creates the graph 
+num_GAP <- as.numeric(as.vector(dataset$Global_active_power))
+datetime <- as.POSIXct(paste(dataset$Date, dataset$Time), format="%Y-%m-%d %H:%M:%S")
+plot(datetime, num_GAP, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.copy(png, file = "plot2.png")
+dev.off()
